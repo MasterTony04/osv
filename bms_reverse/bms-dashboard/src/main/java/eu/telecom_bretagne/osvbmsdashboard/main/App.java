4a47b7a49c76d0b190f77f1caf1609061730606c
Guillaume Le Gall <guillaume.legall@telecom-bretagne.eu>
2016-05-24 12:32:40 +0200

    Started to implement a dashboard for the OSV's BMS.

---
+++ bms_reverse/bms-dashboard/src/main/java/eu/telecom_bretagne/osvbmsdashboard/main/App.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2016 Institut Mines-Télécom
+ *
+ * This file is part of osv-bms-dashboard.
+ *
+ * osv-bms-dashboard is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * osv-bms-dashboard is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with osv-bms-dashboard.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+package eu.telecom_bretagne.osvbmsdashboard.main;
+
+/**
+ * @author guillaumelg
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
