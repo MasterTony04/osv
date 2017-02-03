1b041fc2ca5ffc16fcdf38cca8e5182b17ccf178
Guillaume Le Gall <guillaume.legall@telecom-bretagne.eu>
2017-02-03 22:07:58 +0100

    Fixed names and licenses in source files.

--- dashboard/src/fr/enstb/osv/dashboard/components/OSVPanel.java
+++ dashboard/src/fr/enstb/osv/dashboard/components/OSVPanel.java
@@ -1,7 +1,28 @@
-/**
- * 
+/*
+ * OSVPanel.java
+ *
+ *
+ * Copyright 2017 Institut Mines-Télécom
+ *
+ * This file is part of tb-osv-dashboard.
+ *
+ * tb-osv-dashboard is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * tb-osv-dashboard is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with tb-osv-dashboard. If not, see <http://www.gnu.org/licenses/>.
  */
+
 package fr.enstb.osv.dashboard.components;
+
+import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
@@ -18,5 +39,11 @@ public class OSVPanel extends JPanel {
 
 	public OSVPanel(MainWindow mw) {
 		this.mw = mw;
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawImage(mw.wallpaper, 0, 0, null);
 	}
 }
